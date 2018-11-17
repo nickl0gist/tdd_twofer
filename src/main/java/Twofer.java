a0fb5bf559f268b5363ca5e4efc3fda95104fcf2
Michał Nowotka <mmmnow@gmail.com>
2018-11-17 20:40:54 +0100

    Update Twofer.java

--- src/main/java/Twofer.java
+++ src/main/java/Twofer.java
@@ -1,10 +1,5 @@
 class Twofer {
     String twofer(String name) {
-        String template = "One for %s, one for me.";
-        String actual_name = "you";
-        if (name != null){
-            actual_name = name;
-        }
-        return String.format(template, actual_name);
+        throw new UnsupportedOperationException("Delete this line and write your own implementation");
     }
 }
